f59720c0bb44380b88904e71176b88958b45e493
Mosayeb Alipour <mosaieb@gmail.com>
2025-06-18 16:20:31 +0330

    contin1

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,9 +1,9 @@
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 function swaggerConfig(app) {
     const swaggerDocument = swaggerJsDoc({
         swaggerDefinition: {
-            openapi: "3.1.0",
+            openapi: "3.0.1",
             info: {
                 title: 'My Application Divar API',
                 version: '1.0.0',
@@ -14,7 +14,7 @@ function swaggerConfig(app) {
     });
     const swagger = swaggerUi.setup(swaggerDocument, {});
     // Serve Swagger UI
-      app.use("/",swaggerUi.serve, swagger);
+      app.use("/swa",swaggerUi.serve, swagger);
 }
 
 module.exports = swaggerConfig;
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,6 +1,6 @@
 const {Router} = require ("express");
-const authController = require("./auth.controller");
 const router = Router();
+const authController = require("./auth.controller");
 router.post("/send-otp",authController.sendOTP);
 router.post("/send-otp",authController.checkOTP);
 module.exports ={
