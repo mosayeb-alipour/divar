5f826d214e760048cf829d426c44c0181813afe5
Mosayeb Alipour <mosaieb@gmail.com>
2025-06-29 23:38:52 +0330

    update option controller and option service

--- src/modules/option/option.controller.js
+++ src/modules/option/option.controller.js
@@ -6,10 +6,15 @@ class OptionController{
     #service;
     constructor(){
         autoBind(this);
-        this.#service = OptionService;
+        this.#service = optionService
     }
     async create(req,res,next){
         try {
+            const {title, key, guid, enum: list, type, category} = req.boy;
+            await this.#service.create({title, key, guid, enum: list, type, category})
+            return res.status(HttpCodes.CREATED).json({
+                message: OptionMessage.Created
+            })
 
         } catch (error) {
             next(error)
--- src/modules/option/option.service.js
+++ src/modules/option/option.service.js
@@ -1,18 +1,25 @@
 const autoBind = require("auto-bind");
 const OptionModel = require("./option.model");
+const createHttpError = require("http-errors");
+const { OptionMessage } = require("./option.message");
+const { default: slugify } = require("slugify");
 class OptionService {
     #model;
-    #optionModel;
-    constructor(){
+        constructor(){
         autoBind(this);
         this.#model = OptionModel;
     }
     async find(){
     }
     async create(optionDto) {
+        const category = await this.checkExistById(optionDto.category);
+        optionDto.ket = slugify(optionDto.key,{trim:  true, replacement: "_"})
 
     }
     async checkExistById(id) {
+        const category = await this.#model.findById(id);
+        if(!category) throw new createHttpError.NotFound(OptionMessage.NotFound)
+            return category;
 
     }
 }
