a386cad35bc8fdcfd921e855531410c8ee4a9a69
Mosayeb Alipour <mosaieb@gmail.com>
2025-06-29 19:09:56 +0330

    create swagger option

---
+++ src/modules/option/option.swagger.js
@@ -0,0 +1,75 @@
+/**
+ * @swagger
+ * tags:
+ *  name: Option
+ *  description: Option Module and Routes
+ */
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          CreateOption:
+ *              type: object
+ *              required:
+ *                  -   title
+ *                  -   key
+ *                  -   type
+ *                  -   category
+ *              properties:
+ *                  title:
+ *                      type: string
+ *                  key:
+ *                      type: string
+ *                  category:
+ *                      type: string
+ *                  guid:
+ *                      type: string
+ *                  type:
+ *                      type: string
+ *                      enum:
+ *                          -   number
+ *                          -   string
+ *                          -   boolean
+ *                          -   array
+ *                  enum:
+ *                      type: array
+ *                      items:
+ *                          type: string
+ */
+
+/**
+ * @swagger
+ * 
+ * /option:
+ *  post:
+ *      summary: ctreate new option for category
+ *      tags:
+ *          -   Option
+ *      requestBody:
+ *          content:
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      $ref:   '#/components/schemas/CreateOption'
+ *              application/json:
+ *                  schema:
+ *                      $ref:   '#/components/schemas/CreateOption'
+ *      responses:
+ *          201:
+ *              description: created
+ */
+/**
+ * @swagger
+ * /option/{categoryId}:
+ *  get:
+ *      summary: get all  options of category
+ *      tags:
+ *          -   Option
+ *      parameters:
+ *          -   in: path
+ *              name: categoryId
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: successfully
+ */
