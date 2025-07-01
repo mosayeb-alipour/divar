3a701054ab20855e32c0fcaec7c3703e4d955ea0
Mosayeb Alipour <mosaieb@gmail.com>
2025-07-01 23:56:15 +0330

    ...

--- src/modules/option/option.service.js
+++ src/modules/option/option.service.js
@@ -1,25 +1,40 @@
 const autoBind = require("auto-bind");
 const OptionModel = require("./option.model");
+const CategoryModel = require("./../category/category.model");
 const createHttpError = require("http-errors");
 const { OptionMessage } = require("./option.message");
 const { default: slugify } = require("slugify");
 class OptionService {
     #model;
+    #categoryModel;
         constructor(){
         autoBind(this);
         this.#model = OptionModel;
+        this.#categoryModel = CategoryModel;
     }
     async find(){
     }
     async create(optionDto) {
         const category = await this.checkExistById(optionDto.category);
-        optionDto.ket = slugify(optionDto.key,{trim:  true, replacement: "_"})
+        optionDto.category = category._id
+        c = slugify(optionDto.key,{trim:  true, replacement: "_", lower: true});
+        await this.alreadyExistByCategoryAndKey(optionDto.key,category._id)
+        if(optionDto?.enum && typeof optionDto.enum === "string"){
+            optionDto.enum = optionDto.enum.split(",")
+        }else if (Array.isArray(optionDto.enum)) optionDto.enum = [];
+        const option = await this.#model.create(optionDto)
 
     }
     async checkExistById(id) {
-        const category = await this.#model.findById(id);
+        const category = await this.#categoryModel.findById(id);
         if(!category) throw new createHttpError.NotFound(OptionMessage.NotFound)
             return category;
+
+    }
+    async alreadyExistByCategoryAndKey(key, category) {
+        const isExist = await this.#model.findOne({category,key});
+        if(isExist) throw new createHttpError.Conflict(OptionMessage.AlreadyExist);
+            return null;
 
     }
 }
